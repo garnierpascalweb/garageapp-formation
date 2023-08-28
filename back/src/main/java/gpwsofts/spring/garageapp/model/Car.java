70d1a26e141dd75c178bd49abc5356bcbfb83713
Garnier <pascalscn@hotmail.com>
2023-08-28 19:48:49 +0200

    0.0.1: back: model: ecriture du model Car

---
+++ back/src/main/java/gpwsofts/spring/garageapp/model/Car.java
@@ -0,0 +1,44 @@
+package gpwsofts.spring.garageapp.model;
+
+/**
+ * 
+ * @author Pascal
+ *
+ */
+public class Car {
+	private String model;
+	private String brand;
+	private int year;
+	private Color color;
+	
+	
+	
+	enum Color {
+		RED,BLUE,GREEN,YELLOW
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+}
