584fcd7ca970c119b224133b1a551012793e7ca8
Garnier <pascalscn@hotmail.com>
2023-09-02 11:11:40 +0200

    0.0.1: back: entity : ajout de @Id

--- back/src/main/java/gpwsofts/spring/garageapp/model/Car.java
+++ back/src/main/java/gpwsofts/spring/garageapp/model/Car.java
@@ -2,6 +2,7 @@ package gpwsofts.spring.garageapp.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * 
@@ -11,6 +12,7 @@ import javax.persistence.Entity;
 // Entity (objet Database)
 @Entity
 public class Car {
+	@Id
 	private int id;
 	private String model;
 	private String brand;
