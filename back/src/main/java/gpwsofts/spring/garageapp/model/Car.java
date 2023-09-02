ababd549da0feb062b1f78f8e3c240e73d3cc91e
Garnier <pascalscn@hotmail.com>
2023-09-02 11:22:54 +0200

    0.0.1: back: persistence : mise a jour des notes

--- back/src/main/java/gpwsofts/spring/garageapp/model/Car.java
+++ back/src/main/java/gpwsofts/spring/garageapp/model/Car.java
@@ -2,7 +2,9 @@ package gpwsofts.spring.garageapp.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * 
@@ -13,6 +15,7 @@ import javax.persistence.Id;
 @Entity
 public class Car {
 	@Id
+	@GeneratedValue
 	private int id;
 	private String model;
 	private String brand;
