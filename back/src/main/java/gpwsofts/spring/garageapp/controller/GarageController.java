c8a956eebb34514ee5ea7277fa5e554662ed2f8f
Garnier <pascalscn@hotmail.com>
2023-09-02 12:37:56 +0200

    0.0.1: back: ajout de traces minimalistes avec slf4j

--- back/src/main/java/gpwsofts/spring/garageapp/controller/GarageController.java
+++ back/src/main/java/gpwsofts/spring/garageapp/controller/GarageController.java
@@ -2,6 +2,8 @@ package gpwsofts.spring.garageapp.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,31 +17,38 @@ import gpwsofts.spring.garageapp.service.GarageService;
 @RestController
 public class GarageController {
 	
+	Logger logger = LoggerFactory.getLogger(GarageController.class);
+	
 	@Autowired
 	private GarageService garageService;
 		
 	@RequestMapping(method = RequestMethod.GET, value = "/cars")
 	public List<Car> getCars(){
+		logger.info("appel GET getCars()");
 		return garageService.getCars();
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value="/car/{id}")
 	public Car getCar(@PathVariable int id){
+		logger.info("appel GET getCar(<{}>)", id);
 		return garageService.getCar(id);
 	}
 	
 	@RequestMapping(method = RequestMethod.DELETE, value="/car/{id}")
 	public void deleteCar(@PathVariable int id){
+		logger.info("appel DELETE deleteCar(<{}>)", id);
 		garageService.deleteCar(id);
 	}
 		
 	@RequestMapping(method = RequestMethod.POST, value="/cars")
 	public void addCar(@RequestBody Car car){
+		logger.info("appel POST addCar(<{}>)", car);
 		garageService.addCar(car);
 	}
 	
 	@RequestMapping(method = RequestMethod.PUT, value="/cars/{id}")
 	public void updateCar(@PathVariable int id, @RequestBody Car car){
+		logger.info("appel PUT updateCar(<{}>)", car);
 		garageService.updateCar(id, car);
 	}
 
